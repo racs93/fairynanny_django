ef3366644cc5925dd66ba31579895167e043d7c9
Rodrigo <rodrigocorrea2@hotmail.com>
2019-10-09 15:16:43 -0500

    trying new template

--- fairy_nanny/fairyfront/static/fairyfront/js/google-map.js
+++ fairy_nanny/fairyfront/static/fairyfront/js/google-map.js
@@ -1,208 +1,58 @@
+	var marker;
+	  
+    function initMap() {
+        var myLatLng = {lat: 39.79, lng: -86.14};
 
-var google;
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
-    
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 7,
-
-        // The latitude and longitude to center the map (always required)
-        center: myLatlng,
-
-        // How you would like to style the map. 
-        scrollwheel: false,
-        styles: [
-				  {
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#f5f5f5"
-				      }
-				    ]
-				  },
-				  {
-				    "elementType": "labels.icon",
-				    "stylers": [
-				      {
-				        "visibility": "off"
-				      }
-				    ]
-				  },
-				  {
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#616161"
-				      }
-				    ]
-				  },
-				  {
-				    "elementType": "labels.text.stroke",
-				    "stylers": [
-				      {
-				        "color": "#f5f5f5"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "administrative.land_parcel",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#bdbdbd"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "poi",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#eeeeee"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "poi",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#757575"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "poi.park",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#e5e5e5"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "poi.park",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#9e9e9e"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "road",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#ffffff"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "road.arterial",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#757575"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "road.highway",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#dadada"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "road.highway",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#616161"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "road.local",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#9e9e9e"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "transit.line",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#e5e5e5"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "transit.station",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#eeeeee"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "water",
-				    "elementType": "geometry",
-				    "stylers": [
-				      {
-				        "color": "#c9c9c9"
-				      }
-				    ]
-				  },
-				  {
-				    "featureType": "water",
-				    "elementType": "labels.text.fill",
-				    "stylers": [
-				      {
-				        "color": "#9e9e9e"
-				      }
-				    ]
-				  }
-				]
-    };
-
-    
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-    
-    var addresses = ['New York'];
-
-    for (var x = 0; x < addresses.length; x++) {
-        $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
+		// Specify features and elements to define styles.
+        var styleArray = [
+          {
+            featureType: "all",
+            stylers: [
+             { saturation: -80 }
+            ]
+          },{
+            featureType: "road.arterial",
+            elementType: "geometry",
+            stylers: [
+              { hue: "#000000" },
+              { saturation: 50 }
+            ]
+          },{
+            featureType: "poi.business",
+            elementType: "labels",
+            stylers: [
+              { visibility: "off" }
+            ]
+          }
+        ];
+		
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+		   // Apply the map style array to the map.
+          styles: styleArray,
+          zoom: 7
         });
+
+        var directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        });
+
+		// Create a marker and set its position.
+        marker = new google.maps.Marker({
+          map: map,
+		  icon: 'images/map-marker.png',
+		  draggable: true,
+          animation: google.maps.Animation.DROP,
+          position: myLatLng
+        });
+		marker.addListener('click', toggleBounce);
     }
-    
-}
-google.maps.event.addDomListener(window, 'load', init);
+	  
+	function toggleBounce() {
+        if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+    }
